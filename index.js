1e9942bca78be7920aa4049716808a424f0db9c8
LeoStehlik <leo@sheerness-enterprises.co.uk>
2019-02-18 02:18:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2018, Brandon Lehmann, The TurtleCoin Developers
+//
+// Please see the included LICENSE file for more information.
+
+'use strict'
+
+module.exports.DeroGoldd = require('./lib/derogoldd-rpc.js')
+module.exports.TurtleService = require('./lib/service-rpc.js')
+
+// These exports will be deprecated in a future version.
+// Continue to use them at your own risk.
+module.exports.Walletd = require('./lib/service-rpc.js')
+module.exports.Service = require('./lib/service-rpc.js')
+module.exports.Client = require('./lib/derogoldd-rpc.js')
